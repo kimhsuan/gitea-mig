97c319ee80a10cdebb306741ed69186d7a56a266
Kim <22579665+kimhsuan@users.noreply.github.com>
2026-03-20 19:15:09 +0000

    chore: remove deprecated instance and workload identity federation configurations

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "workload_identity_pool_provider_name" {
-  description = "The name of the workload identity pool provider from the github module."
-  value       = var.environment == "prod" ? module.workload_identity_federation_github[0].workload_identity_pool_provider_names["my-repo"] : null
-}
-
-output "workload_identity_pool_name" {
-  description = "The name of the workload identity pool from the github module."
-  value       = var.environment == "prod" ? module.workload_identity_federation_github[0].workload_identity_pool_name : null
-}
