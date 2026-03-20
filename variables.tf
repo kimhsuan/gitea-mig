62786215d77eba3866ab11dcb2781ba045f24269
Kim <22579665+kimhsuan@users.noreply.github.com>
2026-03-20 19:42:12 +0000

    refactor: enhance variable descriptions for clarity and consistency

--- variables.tf
+++ variables.tf
@@ -1,55 +1,63 @@
 variable "project_id" {
-  type        = string
   description = "The project ID for the Google Cloud resources."
+  type        = string
 }
 
 variable "org_name" {
-  type = string
+  description = "The organization name used for naming and labeling resources."
+  type        = string
 }
 
 variable "app_name" {
-  type    = string
-  default = "gitea"
+  description = "The application name used for naming resources."
+  type        = string
+  default     = "gitea"
 }
 
 variable "environment" {
-  type    = string
-  default = "test"
+  description = "The deployment environment name."
+  type        = string
+  default     = "test"
 }
 
 variable "region" {
-  type        = string
   description = "The region where the resources will be deployed."
+  type        = string
   default     = "us-west1"
 }
 
 variable "zone" {
+  description = "The zone where the instance will be created."
   type        = string
-  description = "The zone to create the instance in"
   default     = null
 }
 
 variable "spot" {
-  type    = bool
-  default = false
+  description = "Whether to use spot capacity for the instance."
+  type        = bool
+  default     = false
 }
 
 variable "source_image_project" {
-  type    = string
-  default = "cos-cloud"
+  description = "The project that provides the source image family for the instance."
+  type        = string
+  default     = "cos-cloud"
 }
 
 variable "source_image_family" {
-  type    = string
-  default = "cos-125-lts"
+  description = "The source image family used to create the instance boot disk."
+  type        = string
+  default     = "cos-125-lts"
 }
 
 variable "github_repository_owner" {
-  type = string
+  description = "The GitHub repository owner used for workload identity federation configuration."
+  type        = string
 }
 
 variable "github_repository" {
-  type = string
+  description = "The GitHub repository name used for workload identity federation configuration."
+  type        = string
 }
 
 variable "ssh_keys" {
@@ -59,53 +67,61 @@ variable "ssh_keys" {
 }
 
 variable "additional_metadata" {
+  description = "Additional metadata to attach to the instance."
   type        = map(any)
-  description = "Additional metadata to attach to the instance"
   default     = {}
 }
 
 variable "docker_image_name" {
-  type    = string
-  default = "docker"
+  description = "The Docker CLI image name used for container management tasks."
+  type        = string
+  default     = "docker"
 }
 
 variable "docker_image_tag" {
-  type    = string
-  default = "27-cli"
+  description = "The Docker CLI image tag used for container management tasks."
+  type        = string
+  default     = "27-cli"
 }
 
 variable "gitea_image_name" {
-  description = "The name of the image to use for the instance."
+  description = "The Gitea container image name."
   type        = string
   default     = "docker.gitea.com/gitea"
 }
 
 variable "gitea_image_tag" {
-  description = "The tag of the image to use for the instance."
+  description = "The Gitea container image tag."
   type        = string
   default     = "latest-rootless"
 }
 
 variable "cloudflared_token" {
-  type = string
+  description = "The Cloudflared tunnel token used to authenticate the tunnel client."
+  type        = string
 }
 
 variable "gitea_domain" {
-  type = string
+  description = "The domain name used to expose the Gitea service."
+  type        = string
 }
 
 variable "gitea_root_url" {
-  type = string
+  description = "The externally accessible root URL for the Gitea instance."
+  type        = string
 }
 
 variable "gitea_lfs_jwt_secret" {
-  type = string
+  description = "The JWT secret used by Gitea for Git LFS authentication."
+  type        = string
 }
 
 variable "gitea_internal_token" {
-  type = string
+  description = "The internal token used by Gitea for inter-service authentication."
+  type        = string
 }
 
 variable "gitea_oauth2_jwt_secret" {
-  type = string
+  description = "The JWT secret used by Gitea for OAuth2 operations."
+  type        = string
 }
